8ac9ad8e80632eb543b1f756b40e2d77db32f7be
Michael Lee <Robodoc88@gmail.com>
2014-05-07 00:22:14 -0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,22 @@
+## read file
+df <- read.table("household_power_consumption.txt", sep=";", header=T)
+
+## convert column 1 to Date format
+df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
+
+## extract data from 2007-02-01 and 2007-02-02
+power <- df[df$Date=="2007-02-01"|df$Date=="2007-02-02",]
+
+## convert column 2 to Date-Time format
+power$Time <- paste(power$Date, power$Time)
+power$Time <- strptime(power$Time, format = "%Y-%m-%d %H:%M:%S")
+
+## convert character to numeric in columns 3 to 9
+power[,3:9] <- sapply(power[,3:9], as.numeric)
+
+## draw histogram
+png("plot1.png")
+hist(power$Global_active_power, 
+     col = "red", main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+dev.off()
